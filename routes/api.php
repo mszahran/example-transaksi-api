c2e906477360a102a8e57ca5edff05274c069181
Zahran <muhammadzahran32@gmail.com>
2024-08-07 18:18:15 +0700

    menambah route Home pada API

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/', function () {
+    return 'Hello Transaksi API';
+});
 Route::prefix('v1/')->group(function () {
     Route::post('register', [App\Http\Controllers\Authentication::class, 'register']);
     Route::post('login', [App\Http\Controllers\Authentication::class, 'login']);
